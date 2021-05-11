675bc8a201a20459dc92ddf2284506af8a9d17eb
Furkan Özdemir <62725361+furkanozzdemir@users.noreply.github.com>
2021-05-12 01:19:14 +0300

    Add files via upload

---
+++ hrms/src/test/java/osc/mrhs/MrhsApplicationTests.java
@@ -0,0 +1,13 @@
+package osc.mrhs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrhsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
